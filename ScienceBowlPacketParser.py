656171b40f769a00d7b57af7a69bfb1c3410e3fa
Ishir Garg <gargishir@gmail.com>
2021-09-23 22:29:07 -0700

    Update ScienceBowlPacketParser.py
    Added class for each question in packet
    
    Now parses through a singular packet and adds all the questions in the form of the Questions class to array
    
    Need to output questions to files based on their question category now

--- ScienceBowlPacketParser.py
+++ ScienceBowlPacketParser.py
@@ -30,8 +30,64 @@ def splitApartPages(organizedPacket):
         else: #If we need a new page to be added
             organizedPacket.append([contents[i]])
         
-
+'''
 splitApartPages(organizedPacket)
 for page in organizedPacket:
     print(page)
     print("\n\n\n")
+'''
+class Question:
+    roundNumber = 0
+    pageNumber = 0
+    questionNumber = 0
+    #First is bonus/toss up, second is mcq/short answer
+    questionTypeA = ""
+    questionTypeB = ""
+    questionCategory = ""
+    questionText = ""
+    questionAnswer = ""
+
+    def __init__ (self, roundNumber, pageNumber, questionNumber, questionTypeA, questionTypeB, questionCategory, questionText, questionAnswer):
+        self.roundNumber = roundNumber
+        self.pageNumber = pageNumber
+        self.questionNumber = questionNumber
+        self.questionTypeA = questionTypeA
+        self.questionTypeB = questionTypeB
+        self.questionCategory = questionCategory
+        self.questionText = questionText
+        self.questionAnswer = questionAnswer
+
+for i in range(0,len(contents)):
+    #Check if it is toss-up question
+    newQuestion = Question(0, 0, 0, "", "", "", "", "")
+    #print(contents[i])
+    if ("TOSS-UP" in contents[i]):
+        newQuestion.questionTypeA = "TOSS-UP"
+    elif ("BONUS" in contents[i]):
+        newQuestion.questionTypeA = "BONUS"
+    if newQuestion.questionTypeA == "TOSS-UP" or newQuestion.questionTypeA == "BONUS":
+        print(newQuestion.questionTypeA)
+        newQuestion.questionNumber = contents[i+1][0]
+        print(newQuestion.questionNumber)
+        newQuestion.questionCategory = readUntilString(contents[i+1], 3, "–")
+        print(newQuestion.questionCategory)
+        startIndexOfQuestionType = contents[i+1].find("–") + 2#Assumes first hyphen is after category name, +2 accounts for space after hypen
+        if (contents[i+1][startIndexOfQuestionType:startIndexOfQuestionType+15] == "Multiple Choice"): #There are 15 char in "Multiple Choice"
+            newQuestion.questionTypeB = "Multiple Choice"
+            print(newQuestion.questionTypeB)
+            fullQuestion = contents[i+1] + contents[i+2] + contents[i+3] + contents[i+4] + contents[i+5]
+            newQuestion.questionText = readUntilString(fullQuestion,startIndexOfQuestionType+15,"ANSWER")
+            print(newQuestion.questionText)
+            newQuestion.questionAnswer = fullQuestion[fullQuestion.find("Answer"):]
+            print(newQuestion.questionAnswer)
+        else:
+            newQuestion.questionTypeB = "Short Answer"
+            print(newQuestion.questionTypeB)
+            newQuestion.questionText = readUntilString(contents[i+1] + contents[i+2],startIndexOfQuestionType+12,"ANSWER")
+            print(newQuestion.questionText)
+            newQuestion.questionAnswer = contents[i+2]
+            print(newQuestion.questionAnswer)
+        questions.append(newQuestion)
+        print("\n\n")
+
+
